43ae3ca307577eada0cee4fa77bc5a068d4307d6
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-03 15:31:58 +0530

    Movies List

--- movies-web/src/app/app.component.ts
+++ movies-web/src/app/app.component.ts
@@ -8,31 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   title = 'Movies Web';
-  movies: any;
 
   ngOnInit(): void {
-
-    setTimeout(() => {
-      this.movies = [
-        {
-          title: 'The Shawshank Redemption',
-          releaseDate: new Date('1994-09-23'),
-          price: 10.99
-        },
-        {
-          title: 'The Godfather',
-          releaseDate: new Date('1972-03-24'),
-          price: 11.99
-        }
-      ];
-    }, 2000);
-
-  }
-
-
-
-  square(n: number) {
-    return n * n;
   }
 
 }
--- movies-web/src/app/movies/movies-list/movies-list.component.ts
+++ movies-web/src/app/movies/movies-list/movies-list.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesListComponent implements OnInit {
 
+  movies: any;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    setTimeout(() => {
+      this.movies = [
+        {
+          title: 'The Shawshank Redemption',
+          releaseDate: new Date('1994-09-23'),
+          price: 10.99
+        },
+        {
+          title: 'The Godfather',
+          releaseDate: new Date('1972-03-24'),
+          price: 11.99
+        }
+      ];
+    }, 2000);
+
   }
 
 }
