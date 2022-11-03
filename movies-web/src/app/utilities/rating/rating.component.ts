bee2bd88a11af76d0c169306be151c3f3e91bf0d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-03 17:54:05 +0530

    Rating Component

--- movies-web/src/app/utilities/rating/rating.component.ts
+++ movies-web/src/app/utilities/rating/rating.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-rating',
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RatingComponent implements OnInit {
 
+  @Input()
+  maxRating = 5;
+  @Input()
+  selectedRate = 0;
+  @Output()
+  onRating: EventEmitter<number> = new EventEmitter<number>();
+  previousRate = 0;
+  maxRatingArr: any[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.maxRatingArr = Array(this.maxRating).fill(0);
+  }
+
+  handleMouseEnter(index: number) {
+    this.selectedRate = index + 1;
+  }
+
+  handleMouseLeave() {
+    if (this.previousRate !== 0) {
+      this.selectedRate = this.previousRate;
+    } else {
+      this.selectedRate = 0;
+    }
+  }
+
+  rate(index: number) {
+    this.selectedRate = index + 1;
+    this.previousRate = this.selectedRate;
+    this.onRating.emit(this.selectedRate);
   }
 
 }
