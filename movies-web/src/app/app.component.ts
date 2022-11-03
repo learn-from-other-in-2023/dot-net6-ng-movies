3f980349ad986ec101637067320b6fb226744018
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-03 15:09:00 +0530

    Using ngFor

--- movies-web/src/app/app.component.ts
+++ movies-web/src/app/app.component.ts
@@ -8,11 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Movies Web';
 
-  movie = {
-    title: 'The Shawshank Redemption',
-    releaseDate: new Date('1994-09-23'),
-    price: 10.99
-  }
+  movies = [
+    {
+      title: 'The Shawshank Redemption',
+      releaseDate: new Date('1994-09-23'),
+      price: 10.99
+    },
+    {
+      title: 'The Godfather',
+      releaseDate: new Date('1972-03-24'),
+      price: 11.99
+    }
+  ]
 
   square(n: number) {
     return n * n;
