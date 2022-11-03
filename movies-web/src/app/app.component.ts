6bcde88c18488a3d55af33d5d58e20d7b42275f2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-03 15:41:00 +0530

    Accepting the Parameters through Input()

--- movies-web/src/app/app.component.ts
+++ movies-web/src/app/app.component.ts
@@ -8,8 +8,41 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   title = 'Movies Web';
+  moviesInTheaters: any[];
+  moviesFutureReleases: any[];
+
+  constructor() {
+    this.moviesInTheaters = [];
+    this.moviesFutureReleases = [];
+  }
 
   ngOnInit(): void {
+
+    this.moviesInTheaters = [
+      {
+        title: 'The Shawshank Redemption',
+        releaseDate: new Date('1994-09-23'),
+        price: 10.99
+      },
+      {
+        title: 'The Godfather',
+        releaseDate: new Date('1972-03-24'),
+        price: 11.99
+      }
+    ];
+
+    this.moviesFutureReleases = [
+      {
+        title: 'Black Widow',
+        releaseDate: new Date('2021-07-07'),
+        price: 10.99
+      },
+      {
+        title: 'The Godfather II',
+        releaseDate: new Date('1974-12-20'),
+        price: 11.99
+      }
+    ];
   }
 
 }
