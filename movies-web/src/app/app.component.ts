d73b5971ac7ce59239634f3307fb298ccfb89de8
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-03 15:22:49 +0530

    ngIf related changes

--- movies-web/src/app/app.component.ts
+++ movies-web/src/app/app.component.ts
@@ -1,25 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Movies Web';
+export class AppComponent implements OnInit {
 
-  movies = [
-    {
-      title: 'The Shawshank Redemption',
-      releaseDate: new Date('1994-09-23'),
-      price: 10.99
-    },
-    {
-      title: 'The Godfather',
-      releaseDate: new Date('1972-03-24'),
-      price: 11.99
-    }
-  ]
+  title = 'Movies Web';
+  movies: any;
+
+  ngOnInit(): void {
+
+    setTimeout(() => {
+      this.movies = [
+        {
+          title: 'The Shawshank Redemption',
+          releaseDate: new Date('1994-09-23'),
+          price: 10.99
+        },
+        {
+          title: 'The Godfather',
+          releaseDate: new Date('1972-03-24'),
+          price: 11.99
+        }
+      ];
+    }, 2000);
+
+  }
+
+
 
   square(n: number) {
     return n * n;
