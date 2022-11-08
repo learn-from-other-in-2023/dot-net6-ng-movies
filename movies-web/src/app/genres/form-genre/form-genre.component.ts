2b88fbb43a548049da844023727f77d35eca1956
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-08 22:07:55 +0530

    Moved the logic into Form Genre

--- movies-web/src/app/genres/form-genre/form-genre.component.ts
+++ movies-web/src/app/genres/form-genre/form-genre.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { firstLetterUppercase } from 'src/app/validators/firstLetterUppercase';
 
 @Component({
   selector: 'app-form-genre',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormGenreComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup | any;
+
+  constructor(private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+
+    this.form = this.formBuilder.group({
+      name: ['', {
+        validators: [Validators.required, Validators.minLength(3), firstLetterUppercase()]
+      }]
+    });
+
+  }
+
+  saveChanges() {
+    // ... save the genre
+
+    this.router.navigate(['/genres']);
+
+  }
+
+  backToHome() {
+    this.router.navigate(['/']);
+  }
+
+  getErrorMessageFieldName() {
+    const field = this.form.get('name');
+
+    if (field.hasError('required')) {
+      return 'The name field is required';
+    }
+
+    if (field.hasError('minlength')) {
+      return 'The minimum length is 3';
+    }
+
+    if (field.hasError('firstLetterUppercase')) {
+      return field.getError('firstLetterUppercase').message;
+    }
+
+    return '';
   }
 
 }
