2b3b5c7386e228e9b4b3e883f34f6d4ecf911a35
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-08 10:52:59 +0530

    Reading Route Parameters

--- movies-web/src/app/genres/edit-genre/edit-genre.component.ts
+++ movies-web/src/app/genres/edit-genre/edit-genre.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-genre',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditGenreComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      console.log('EditActorComponent: ', params['id']);
+    });
   }
 
 }
