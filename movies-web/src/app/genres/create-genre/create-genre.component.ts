59d53f0b1b43d5c72361e9de84162fe712f4e153
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-08 22:00:53 +0530

    Added Custom Validator

--- movies-web/src/app/genres/create-genre/create-genre.component.ts
+++ movies-web/src/app/genres/create-genre/create-genre.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { firstLetterUppercase } from 'src/app/validators/firstLetterUppercase';
 
 @Component({
   selector: 'app-create-genre',
@@ -10,14 +11,14 @@ import { Router } from '@angular/router';
 export class CreateGenreComponent implements OnInit {
 
   form: FormGroup | any;
-  
+
   constructor(private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
 
     this.form = this.formBuilder.group({
       name: ['', {
-        validators: [Validators.required, Validators.minLength(3)]
+        validators: [Validators.required, Validators.minLength(3), firstLetterUppercase()]
       }]
     });
 
@@ -32,6 +33,24 @@ export class CreateGenreComponent implements OnInit {
 
   backToHome() {
     this.router.navigate(['/']);
+  }
+
+  getErrorMessageFieldName() {
+    const field = this.form.get('name');
+
+    if (field.hasError('required')) {
+      return 'The name field is required';
+    }
+
+    if (field.hasError('minlength')) {
+      return 'The minimum length is 3';
+    }
+
+    if (field.hasError('firstLetterUppercase')) {
+      return field.getError('firstLetterUppercase').message;
+    }
+
+    return '';
   }
 
 }
