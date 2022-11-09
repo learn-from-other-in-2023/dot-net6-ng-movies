6186751b263ae83b90830751bc19b27c7b97d6a2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-09 22:07:39 +0530

    Create Actor Initial Version

--- movies-web/src/app/actors/form-actor/form-actor.component.ts
+++ movies-web/src/app/actors/form-actor/form-actor.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IActorCreationDto } from '../actors.model';
 
 @Component({
   selector: 'app-form-actor',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormActorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
+
+  form: FormGroup | any;
+
+  @Input()
+  model: IActorCreationDto | any;
+
+  @Output()
+  onSaveChanges = new EventEmitter<IActorCreationDto>();
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: ['', {
+        validators: [Validators.required]
+      }],
+      dateOfBirth: '',
+      picture: '',
+      biography: ''
+    });
+
+    if (this.model !== undefined) {
+      this.form.patchValue(this.model);
+    }
+  }
+
+  onImageSelected(image: any) {
+    this.form.get('picture').setValue(image);
+  }
+
+  changeMarkdown(content: any) {
+    this.form.get('biography').setValue(content);
+  }
+
+  saveChanges() {
+    this.onSaveChanges.emit(this.form.value);
   }
 
 }
