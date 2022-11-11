b34ea6313cc9fb0b5b04b3b3f9e94ffebf5bc239
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-11 23:05:48 +0530

    Edit Movie

--- movies-web/src/app/movie-theaters/edit-movie-theater/edit-movie-theater.component.ts
+++ movies-web/src/app/movie-theaters/edit-movie-theater/edit-movie-theater.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IMovieTheatersCreationDto, IMovieTheatersDto } from '../movie-theaters.model';
 
 @Component({
   selector: 'app-edit-movie-theater',
@@ -8,12 +9,18 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EditMovieTheaterComponent implements OnInit {
 
+  movieTheatersDto: IMovieTheatersDto = { name: 'Agora', latitude: 18.483541251864438, longitude: -69.93927597999574 };
+
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params => {
       console.log('EditActorComponent: ', params['id']);
     });
+  }
+
+  saveChanges(movieTheater: IMovieTheatersCreationDto) {
+    console.log(movieTheater);
   }
 
 }
