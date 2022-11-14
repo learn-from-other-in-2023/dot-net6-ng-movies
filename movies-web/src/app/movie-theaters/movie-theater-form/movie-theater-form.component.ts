949b1e9b42a4a7eb261209c8cf6438b9dd3d4632
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-14 08:31:17 +0530

    Swamy/12nov2022 work (#11)
    * Renamed the Variable
    
    * Map Component Initial Version
    
    * Initial Version of Map
    
    * Showing Map using Leaflet
    
    * Showing the Icon on Map
    
    * Setting the initial Coordinates

--- movies-web/src/app/movie-theaters/movie-theater-form/movie-theater-form.component.ts
+++ movies-web/src/app/movie-theaters/movie-theater-form/movie-theater-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ICoordinatesMap } from 'src/app/utilities/map/coordinate';
 import { IMovieTheatersCreationDto, IMovieTheatersDto } from '../movie-theaters.model';
 
 @Component({
@@ -19,31 +20,33 @@ export class MovieTheaterFormComponent implements OnInit {
   @Output()
   onSaveChangesEvent = new EventEmitter<IMovieTheatersCreationDto>();
 
-  // initialCoordinates: coordinatesMap[] = [];
+  initialCoordinates: ICoordinatesMap[] = [];
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
       name: ['', {
         validators: [Validators.required]
       }],
-      longitude: ['1', {
+      longitude: ['78.491684', {
         validators: [Validators.required]
       }],
-      latitude: ['1', {
+      latitude: ['17.387140', {
         validators: [Validators.required]
       }]
     })
 
+    this.initialCoordinates.push({ latitude: 17.387140, longitude: 78.491684 });
+
     if (this.movieTheatersDto !== undefined) {
       this.form.patchValue(this.movieTheatersDto);
 
-      // this.initialCoordinates.push({ latitude: this.model.latitude, longitude: this.model.longitude });
+      this.initialCoordinates.push({ latitude: this.form.latitude, longitude: this.form.longitude });
     }
   }
 
-  // onSelectedLocation(coordinates: coordinatesMap) {
-  //   this.form.patchValue(coordinates);
-  // }
+  onSelectedLocation(coordinates: ICoordinatesMap) {
+    this.form.patchValue(coordinates);
+  }
 
   saveChanges() {
     this.onSaveChangesEvent.emit(this.form.value);
--- movies-web/src/app/utilities/map/map.component.ts
+++ movies-web/src/app/utilities/map/map.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { marker, tileLayer, latLng, Marker, LeafletMouseEvent, icon } from 'leaflet';
+import { ICoordinatesMap } from './coordinate';
 
 @Component({
   selector: 'app-map',
@@ -7,9 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+  @Input()
+  initialCoordinates: ICoordinatesMap[] = [];
+
+  @Output()
+  onSelectedLocation = new EventEmitter<ICoordinatesMap>();
+
+  defaultIcon = {
+    icon: icon({
+      iconSize: [25, 41],
+      iconAnchor: [13, 0],
+      iconRetinaUrl: 'assets/marker-icon-2x.png',
+      iconUrl: 'assets/marker-icon.png',
+      shadowUrl: 'assets/marker-shadow.png'
+    })
+  };
+
+  layers: Marker<any>[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    
+    console.log(this.initialCoordinates);
+    this.layers = this.initialCoordinates.map(value => marker([value.latitude, value.longitude], this.defaultIcon));
+  }
+
+  mapComponentOptions = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: 'Angular Movies'
+      })
+    ],
+    zoom: 14,
+    center: latLng(17.38714, 78.491684)
+  };
+
+  handleMapClick(event: LeafletMouseEvent) {
+    const latitude = event.latlng.lat;
+    const longitude = event.latlng.lng;
+    console.log({ latitude, longitude });
+
+    this.layers = [];
+    this.layers.push(marker([latitude, longitude], this.defaultIcon));
+
+    this.onSelectedLocation.emit({ latitude, longitude });
   }
 
 }
