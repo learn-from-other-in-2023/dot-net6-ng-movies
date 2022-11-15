70d886d2c98a33be4fcdf7395166e83653dc193a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-15 22:53:06 +0530

    Improvements

--- Movies.API/Movies.API/Entities/Genre.cs
+++ Movies.API/Movies.API/Entities/Genre.cs
@@ -1,6 +1,14 @@
-﻿namespace Movies.API.Entities
+﻿using Movies.API.Validations;
+using System.ComponentModel.DataAnnotations;
+
+namespace Movies.API.Entities
 {
+
     public class Genre
     {
+        public int Id { get; set; }
+        
+        public string? Name { get; set; }
     }
+
 }
---
+++ Movies.API/Movies.API/Services/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Movies.API.Entities;
+
+namespace Movies.API.Services
+{
+
+    public interface IRepository
+    {
+        void AddGenre(Genre genre);
+
+        Task<List<Genre>> GetAllGenres();
+
+        Genre GetGenreById(int Id);
+    }
+
+}
