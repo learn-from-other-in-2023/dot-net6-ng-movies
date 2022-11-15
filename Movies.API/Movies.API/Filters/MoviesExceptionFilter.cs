edb2266d31766300996b159fc51f28096c8f46b9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-15 22:48:33 +0530

    Exception Filter

---
+++ Movies.API/Movies.API/Filters/MoviesExceptionFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace Movies.API.Filters
+{
+
+    public class MoviesExceptionFilter : ExceptionFilterAttribute
+    {
+        private readonly ILogger<MoviesExceptionFilter> _logger;
+
+        public MoviesExceptionFilter(ILogger<MoviesExceptionFilter> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public override void OnException(ExceptionContext context)
+        {
+            _logger.LogError(context.Exception, context.Exception.Message);
+
+            base.OnException(context);
+        }
+
+    }
+}
