518fb20c121e55bb437764e191c7fa571c55f8e2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-17 19:10:15 +0530

    Entities

---
+++ Movies.API/Movies.API/Entities/MoviesGenres.cs
@@ -0,0 +1,15 @@
+﻿namespace Movies.API.Entities
+{
+
+    public class MoviesGenres
+    {
+        public int GenreId { get; set; }
+
+        public int MovieId { get; set; }
+
+        public Genre? Genre { get; set; }
+
+        public Movie? Movie { get; set; }
+    }
+
+}
