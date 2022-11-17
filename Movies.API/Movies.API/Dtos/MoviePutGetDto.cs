77e9849ed0663e4f8c7857e0c02fb472db4cde40
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-17 17:57:22 +0530

    Dtos

---
+++ Movies.API/Movies.API/Dtos/MoviePutGetDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Movies.API.Dtos
+{
+
+    public class MoviePutGetDto
+    {
+        public MovieDto? Movie { get; set; }
+
+        public List<GenreDto>? SelectedGenres { get; set; }
+
+        public List<GenreDto>? NonSelectedGenres { get; set; }
+
+        public List<MovieTheaterDto>? SelectedMovieTheaters { get; set; }
+
+        public List<MovieTheaterDto>? NonSelectedMovieTheaters { get; set; }
+
+        public List<ActorsMovieDto>? Actors { get; set; }
+    }
+
+}
---
+++ Movies.API/Movies.API/Dtos/MovieTheaterCreationDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movies.API.Dtos
+{
+
+    public class MovieTheaterCreationDto
+    {
+        [Required]
+        [StringLength(maximumLength: 75)]
+        public string? Name { get; set; }
+
+        [Range(-90, 90)]
+        public double Latitude { get; set; }
+
+        [Range(-180, 180)]
+        public double Longitude { get; set; }
+    }
+
+}
