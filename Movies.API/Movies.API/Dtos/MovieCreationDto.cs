a21345b783d7803f410d0cf566432709385c1d55
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-17 17:49:25 +0530

    Dtos

---
+++ Movies.API/Movies.API/Dtos/MovieCreationDto.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.API.Helpers;
+
+namespace Movies.API.Dtos
+{
+
+    public class MovieCreationDto
+    {
+        public string? Title { get; set; }
+
+        public string? Summary { get; set; }
+
+        public string? Trailer { get; set; }
+
+        public bool InTheaters { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public IFormFile? Poster { get; set; }
+
+        [ModelBinder(BinderType = typeof(TypeBinder<List<int>>))]
+        public List<int>? GenresIds { get; set; }
+
+        [ModelBinder(BinderType = typeof(TypeBinder<List<int>>))]
+        public List<int>? MovieTheatersIds { get; set; }
+
+        [ModelBinder(BinderType = typeof(TypeBinder<List<MoviesActorsCreationDto>>))]
+        public List<MoviesActorsCreationDto>? Actors { get; set; }
+    }
+
+}
--- Movies.API/Movies.API/Dtos/MovieDto.cs
+++ Movies.API/Movies.API/Dtos/MovieDto.cs
@@ -1,6 +1,27 @@
 ﻿namespace Movies.API.Dtos
 {
+
     public class MovieDto
     {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Summary { get; set; }
+
+        public string? Trailer { get; set; }
+
+        public bool? InTheaters { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string? Poster { get; set; }
+
+        public List<GenreDto>? Genres { get; set; }
+
+        public List<MovieTheaterDto>? MovieTheaters { get; set; }
+
+        public List<ActorsMovieDto>? Actors { get; set; }
     }
+
 }
