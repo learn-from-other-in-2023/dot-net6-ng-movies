c44741a43e01a2255cc43c512086fb9d1bd0d563
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-20 15:20:03 +0530

    Another Database Migration

--- Movies.API/Movies.API/Entities/MovieTheatersMovies.cs
+++ Movies.API/Movies.API/Entities/MovieTheatersMovies.cs
@@ -5,9 +5,9 @@
     {
         public int MovieTheaterId { get; set; }
 
-        public int MovieId { get; set; }
-
         public MovieTheater? MovieTheater { get; set; }
+
+        public int MovieId { get; set; }
 
         public Movie? Movie { get; set; }
     }
--- Movies.API/Movies.API/Entities/MoviesGenres.cs
+++ Movies.API/Movies.API/Entities/MoviesGenres.cs
@@ -5,9 +5,9 @@
     {
         public int GenreId { get; set; }
 
-        public int MovieId { get; set; }
-
         public Genre? Genre { get; set; }
+
+        public int MovieId { get; set; }
 
         public Movie? Movie { get; set; }
     }
