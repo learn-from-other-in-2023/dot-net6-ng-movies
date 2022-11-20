6163fe7f257e679410907194285bd90fcd717028
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-20 15:44:32 +0530

    Movies Controller

--- Movies.API/Movies.API/Configuration/MappingConfig.cs
+++ Movies.API/Movies.API/Configuration/MappingConfig.cs
@@ -12,13 +12,6 @@ namespace Movies.API.Configuration
     public class MappingConfig
     {
 
-        //private readonly GeometryFactory _geometryFactory;
-
-        //public MappingConfig(GeometryFactory geometryFactory)
-        //{
-        //    _geometryFactory = geometryFactory ?? throw new ArgumentNullException(nameof(geometryFactory));
-        //}
-
         /// <summary>
         /// 
         /// </summary>
@@ -44,9 +37,129 @@ namespace Movies.API.Configuration
                    .ForMember(x => x.Location, x => x.MapFrom(dto =>
                    geometryFactory.CreatePoint(new Coordinate(dto.Longitude, dto.Latitude))));
 
+                _ = config.CreateMap<MovieCreationDto, Movie>()
+                .ForMember(x => x.Poster, options => options.Ignore())
+                .ForMember(x => x.MoviesGenres, options => options.MapFrom(MapMoviesGenres))
+                .ForMember(x => x.MovieTheatersMovies, options => options.MapFrom(MapMovieTheatersMovies))
+                .ForMember(x => x.MoviesActors, options => options.MapFrom(MapMoviesActors));
+
+                //CreateMap<Movie, MovieDTO>()
+                //    .ForMember(x => x.Genres, options => options.MapFrom(MapMoviesGenres))
+                //    .ForMember(x => x.MovieTheaters, options => options.MapFrom(MapMovieTheatersMovies))
+                //    .ForMember(x => x.Actors, options => options.MapFrom(MapMoviesActors));
+
             });
 
             return mappingConfig;
+        }
+
+        private static List<ActorsMovieDto> MapMoviesActors(Movie movie, MovieDto movieDto)
+        {
+            var result = new List<ActorsMovieDto>();
+
+            if (movie.MoviesActors is not null)
+            {
+                foreach (var moviesActors in movie.MoviesActors)
+                {
+                    result.Add(new ActorsMovieDto()
+                    {
+                        Id = moviesActors.ActorId,
+                        Name = moviesActors?.Actor?.Name,
+                        Character = moviesActors?.Character,
+                        Picture = moviesActors?.Actor?.Picture,
+                        Order = moviesActors.Order
+                    });
+                }
+            }
+
+            return result;
+        }
+
+        private static List<MovieTheaterDto> MapMovieTheatersMovies(Movie movie, MovieDto movieDto)
+        {
+            var result = new List<MovieTheaterDto>();
+
+            if (movie.MovieTheatersMovies is not null)
+            {
+                foreach (var movieTheaterMovies in movie.MovieTheatersMovies)
+                {
+                    result.Add(new MovieTheaterDto()
+                    {
+                        Id = movieTheaterMovies.MovieTheaterId,
+                        Name = movieTheaterMovies?.MovieTheater?.Name,
+                        Latitude = movieTheaterMovies.MovieTheater.Location.Y,
+                        Longitude = movieTheaterMovies.MovieTheater.Location.X
+                    });
+                }
+            }
+
+            return result;
+        }
+
+        private static List<GenreDto> MapMoviesGenres(Movie movie, MovieDto movieDto)
+        {
+            var result = new List<GenreDto>();
+
+            if (movie.MoviesGenres != null)
+            {
+                foreach (var genre in movie.MoviesGenres)
+                {
+                    result.Add(new GenreDto() { Id = genre.GenreId, Name = genre?.Genre?.Name });
+                }
+            }
+
+            return result;
+        }
+
+        private static List<MoviesGenres> MapMoviesGenres(MovieCreationDto movieCreationDto, Movie movie)
+        {
+            var result = new List<MoviesGenres>();
+
+            if (movieCreationDto.GenresIds is null)
+            {
+                return result;
+            }
+
+            foreach (var id in movieCreationDto.GenresIds)
+            {
+                result.Add(new MoviesGenres() { GenreId = id });
+            }
+
+            return result;
+        }
+
+        private static List<MovieTheatersMovies> MapMovieTheatersMovies(MovieCreationDto movieCreationDto, Movie movie)
+        {
+            var result = new List<MovieTheatersMovies>();
+
+            if (movieCreationDto.MovieTheatersIds is null)
+            {
+                return result;
+            }
+
+            foreach (var id in movieCreationDto.MovieTheatersIds)
+            {
+                result.Add(new MovieTheatersMovies() { MovieTheaterId = id });
+            }
+
+            return result;
+        }
+
+        private static List<MoviesActors> MapMoviesActors(MovieCreationDto movieCreationDto, Movie movie)
+        {
+            var result = new List<MoviesActors>();
+
+            if (movieCreationDto.Actors is null)
+            {
+                return result;
+            }
+
+            foreach (var actor in movieCreationDto.Actors)
+            {
+                result.Add(new MoviesActors() { ActorId = actor.Id, Character = actor.Character });
+            }
+
+            return result;
         }
 
     }
--- Movies.API/Movies.API/Controllers/MoviesController.cs
+++ Movies.API/Movies.API/Controllers/MoviesController.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Movies.API.Dtos;
+using Movies.API.Entities;
 using Movies.API.Helpers;
 using Movies.API.Persistance;
 
@@ -24,6 +27,208 @@ namespace Movies.API.Controllers
 
             this.fileStorageService = fileStorageService ?? throw new ArgumentNullException(nameof(fileStorageService));
         }
+
+        [HttpGet]
+        public async Task<ActionResult<HomeDto>> Get()
+        {
+            var top = 6;
+            var today = DateTime.Today;
+
+            var upcomingReleases = await context.Movies
+                .Where(x => x.ReleaseDate > today)
+                .OrderBy(x => x.ReleaseDate)
+                .Take(top)
+                .ToListAsync();
+
+            var inTheaters = await context.Movies
+                .Where(x => x.InTheaters)
+                .OrderBy(x => x.ReleaseDate)
+                .Take(top)
+                .ToListAsync();
+
+            var homeDto = new HomeDto
+            {
+                UpcomingReleases = mapper.Map<List<MovieDto>>(upcomingReleases),
+                InTheaters = mapper.Map<List<MovieDto>>(inTheaters)
+            };
+
+            return homeDto;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<MovieDto>> Get(int id)
+        {
+            var movie = await context.Movies
+                .Include(x => x.MoviesGenres).ThenInclude(x => x.Genre)
+                .Include(x => x.MovieTheatersMovies).ThenInclude(x => x.MovieTheater)
+                .Include(x => x.MoviesActors).ThenInclude(x => x.Actor)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            var dto = mapper.Map<MovieDto>(movie);
+            dto.Actors = dto?.Actors?.OrderBy(x => x.Order).ToList();
+
+            return dto;
+        }
+
+        [HttpGet("filter")]
+        public async Task<ActionResult<List<MovieDto>>> Filter([FromQuery] FilterMoviesDto filterMoviesDTO)
+        {
+            var moviesQueryable = context.Movies.AsQueryable();
+
+            if (!string.IsNullOrEmpty(filterMoviesDTO.Title))
+            {
+                moviesQueryable = moviesQueryable.Where(x => x.Title.Contains(filterMoviesDTO.Title));
+            }
+
+            if (filterMoviesDTO.InTheaters)
+            {
+                moviesQueryable = moviesQueryable.Where(x => x.InTheaters);
+            }
+
+            if (filterMoviesDTO.UpcomingReleases)
+            {
+                var today = DateTime.Today;
+                moviesQueryable = moviesQueryable.Where(x => x.ReleaseDate > today);
+            }
+
+            if (filterMoviesDTO.GenreId != 0)
+            {
+                moviesQueryable = moviesQueryable
+                    .Where(x => x.MoviesGenres.Select(y => y.GenreId)
+                    .Contains(filterMoviesDTO.GenreId));
+            }
+
+            await HttpContext.InsertParametersPaginationInHeader(moviesQueryable);
+            var movies = await moviesQueryable.OrderBy(x => x.Title)
+                .Paginate(filterMoviesDTO.PaginationDto)
+                .ToListAsync();
+
+            return mapper.Map<List<MovieDto>>(movies);
+        }
+
+        [HttpGet("PostGet")]
+        public async Task<ActionResult<MoviePostGetDto>> PostGet()
+        {
+            var movieTheaters = await context.MovieTheaters.OrderBy(x => x.Name).ToListAsync();
+            var genres = await context.Genres.OrderBy(x => x.Name).ToListAsync();
+
+            var movieTheatersDTO = mapper.Map<List<MovieTheaterDto>>(movieTheaters);
+            var genresDTO = mapper.Map<List<GenreDto>>(genres);
+
+            return new MoviePostGetDto() { Genres = genresDTO, MovieTheaters = movieTheatersDTO };
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Post([FromForm] MovieCreationDto movieCreationDTO)
+        {
+            var movie = mapper.Map<Movie>(movieCreationDTO);
+
+            if (movieCreationDTO.Poster != null)
+            {
+                movie.Poster = await fileStorageService.SaveFile(container, movieCreationDTO.Poster);
+            }
+
+            AnnotateActorsOrder(movie);
+            context.Add(movie);
+            await context.SaveChangesAsync();
+            return movie.Id;
+        }
+
+        [HttpGet("putget/{id:int}")]
+        public async Task<ActionResult<MoviePutGetDto>> PutGet(int id)
+        {
+            var movieActionResult = await Get(id);
+            if (movieActionResult.Result is NotFoundResult) { return NotFound(); }
+
+            var movie = movieActionResult.Value;
+
+            var genresSelectedIds = movie.Genres.Select(x => x.Id).ToList();
+            var nonSelectedGenres = await context.Genres.Where(x => !genresSelectedIds.Contains(x.Id))
+                .ToListAsync();
+
+            var movieTheatersIds = movie.MovieTheaters.Select(x => x.Id).ToList();
+            var nonSelectedMovieTheaters = await context.MovieTheaters.Where(x =>
+            !movieTheatersIds.Contains(x.Id)).ToListAsync();
+
+            var nonSelectedGenresDTOs = mapper.Map<List<GenreDto>>(nonSelectedGenres);
+            var nonSelectedMovieTheatersDTO = mapper.Map<List<MovieTheaterDto>>(nonSelectedMovieTheaters);
+
+            var response = new MoviePutGetDto
+            {
+                Movie = movie,
+                SelectedGenres = movie.Genres,
+                NonSelectedGenres = nonSelectedGenresDTOs,
+                SelectedMovieTheaters = movie.MovieTheaters,
+                NonSelectedMovieTheaters = nonSelectedMovieTheatersDTO,
+                Actors = movie.Actors
+            };
+
+            return response;
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id, [FromForm] MovieCreationDto movieCreationDTO)
+        {
+            var movie = await context.Movies.Include(x => x.MoviesActors)
+                .Include(x => x.MoviesGenres)
+                .Include(x => x.MovieTheatersMovies)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            movie = mapper.Map(movieCreationDTO, movie);
+
+            if (movieCreationDTO.Poster != null)
+            {
+                movie.Poster = await fileStorageService.EditFile(container, movieCreationDTO.Poster,
+                    movie.Poster);
+            }
+
+            AnnotateActorsOrder(movie);
+
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var movie = await context.Movies.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            context.Remove(movie);
+
+            await context.SaveChangesAsync();
+
+            await fileStorageService.DeleteFile(movie.Poster, container);
+
+            return NoContent();
+        }
+
+        private static void AnnotateActorsOrder(Movie movie)
+        {
+            if (movie.MoviesActors != null)
+            {
+                for (int i = 0; i < movie.MoviesActors.Count; i++)
+                {
+                    movie.MoviesActors[i].Order = i;
+                }
+            }
+        }
+
     }
 
 }
