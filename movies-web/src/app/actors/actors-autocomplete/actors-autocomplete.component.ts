471dcf0b444df8903881c70ad1985aa23ebd7f21
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-20 21:09:03 +0530

    Actors To Display

--- movies-web/src/app/actors/actors-autocomplete/actors-autocomplete.component.ts
+++ movies-web/src/app/actors/actors-autocomplete/actors-autocomplete.component.ts
@@ -1,8 +1,10 @@
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { MatTable } from '@angular/material/table';
+import { IActorsMovieDto } from '../actors.model';
+import { ActorsService } from '../actors.service';
 
 @Component({
   selector: 'app-actors-autocomplete',
@@ -11,48 +13,49 @@ import { MatTable } from '@angular/material/table';
 })
 export class ActorsAutocompleteComponent implements OnInit {
 
-  constructor() { }
-
   control: FormControl = new FormControl();
 
-  actors = [
-    {name: 'Tom Holland', picture: 'https://picsum.photos/200/200?grayscale'},
-    {name: 'Tom Hanks', picture: 'https://picsum.photos/200/200?grayscale'},
-    {name: 'Samuel L. Jackson', picture: 'https://picsum.photos/200/200?grayscale'}
-  ]
+  @Input()
+  selectedActors: IActorsMovieDto[] = [];
 
-  selectedActors: any = [];
-
-  originalActors = this.actors;
+  actorsToDisplay: IActorsMovieDto[] = [];
 
   columnsToDisplay = ['picture', 'name', 'character', 'actions']
 
   @ViewChild(MatTable) table: MatTable<any> | any;
 
+  constructor(private actorsService: ActorsService) { }
+
   ngOnInit(): void {
     this.control.valueChanges.subscribe(value => {
-      this.actors = this.originalActors;
-      this.actors = this.actors.filter(actor => actor.name.indexOf(value) !== -1);
-    })
+      this.actorsService.searchByName(value).subscribe(actors => {
+        this.actorsToDisplay = actors;
+      });
+    });
   }
 
-  optionSelected(event: MatAutocompleteSelectedEvent){
+  optionSelected(event: MatAutocompleteSelectedEvent) {
     console.log(event.option.value);
-    this.selectedActors.push(event.option.value);
     this.control.patchValue('');
-    if (this.table !== undefined){
+
+    if (this.selectedActors.findIndex(x => x.id == event.option.value.id) !== -1) {
+      return;
+    }
+
+    this.selectedActors.push(event.option.value);
+    if (this.table !== undefined) {
       this.table.renderRows();
     }
   }
 
-  remove(actor: any){
-    const index = this.selectedActors.findIndex((a: any) => a.name === actor.name);
+  remove(actor: any) {
+    const index = this.selectedActors.findIndex(a => a.name === actor.name);
     this.selectedActors.splice(index, 1);
     this.table.renderRows();
   }
 
-  dropped(event: CdkDragDrop<any[]>){
-    const previousIndex = this.selectedActors.findIndex((actor: any) => actor === event.item.data);
+  dropped(event: CdkDragDrop<any[]>) {
+    const previousIndex = this.selectedActors.findIndex(actor => actor === event.item.data);
     moveItemInArray(this.selectedActors, previousIndex, event.currentIndex);
     this.table.renderRows();
   }
