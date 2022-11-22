227ef821a90b70d42ce6cd6077550b61f41413eb
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-22 22:42:16 +0530

    Update rating.component.ts

--- movies-web/src/app/utilities/rating/rating.component.ts
+++ movies-web/src/app/utilities/rating/rating.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import Swal from 'sweetalert2';
+import { SecurityService } from '~/app/security/security.service';
 
 @Component({
   selector: 'app-rating',
@@ -20,7 +22,8 @@ export class RatingComponent implements OnInit {
 
   maxRatingArr: any[] = [];
 
-  constructor() { }
+  constructor(private securityService: SecurityService) {
+  }
 
   ngOnInit(): void {
     this.maxRatingArr = Array(this.maxRating).fill(0);
@@ -39,9 +42,13 @@ export class RatingComponent implements OnInit {
   }
 
   rate(index: number) {
-    this.selectedRate = index + 1;
-    this.previousRate = this.selectedRate;
-    this.onRatingEvent.emit(this.selectedRate);
+    if (this.securityService.isAuthenticated()) {
+      this.selectedRate = index + 1;
+      this.previousRate = this.selectedRate;
+      this.onRatingEvent.emit(this.selectedRate);
+    } else {
+      Swal.fire("Information", "You need to log in before voting", "info");
+    }
   }
 
 }
