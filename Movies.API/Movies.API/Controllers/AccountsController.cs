10629b67e25ef73378c8865b6e685ee6f82cee60
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-22 13:47:26 +0530

    Accounts Controller

--- Movies.API/Movies.API/Controllers/AccountsController.cs
+++ Movies.API/Movies.API/Controllers/AccountsController.cs
@@ -3,10 +3,15 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Movies.API.Dtos;
+using Movies.API.Entities;
 using Movies.API.Helpers;
 using Movies.API.Persistance;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Text;
 
 namespace Movies.API.Controllers
 {
@@ -42,7 +47,7 @@ namespace Movies.API.Controllers
             var queryable = context.Users.AsQueryable();
             await HttpContext.InsertParametersPaginationInHeader(queryable);
             var users = await queryable.OrderBy(x => x.Email).Paginate(paginationDTO).ToListAsync();
-            return mapper.Map<List<UserDTO>>(users);
+            return mapper.Map<List<UserDto>>(users);
         }
 
         [HttpPost("makeAdmin")]
@@ -52,6 +57,74 @@ namespace Movies.API.Controllers
             var user = await userManager.FindByIdAsync(userId);
             await userManager.AddClaimAsync(user, new Claim("role", "admin"));
             return NoContent();
+        }
+
+        [HttpPost("removeAdmin")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Policy = "IsAdmin")]
+        public async Task<ActionResult> RemoveAdmin([FromBody] string userId)
+        {
+            var user = await userManager.FindByIdAsync(userId);
+            await userManager.RemoveClaimAsync(user, new Claim("role", "admin"));
+            return NoContent();
+        }
+
+        [HttpPost("create")]
+        public async Task<ActionResult<AuthenticationResponseDto>> Create([FromBody] UserCredentials userCredentials)
+        {
+            var user = new IdentityUser { UserName = userCredentials.Email, Email = userCredentials.Email };
+            var result = await userManager.CreateAsync(user, userCredentials.Password);
+
+            if (result.Succeeded)
+            {
+                return await BuildToken(userCredentials);
+            }
+            else
+            {
+                return BadRequest(result.Errors);
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthenticationResponseDto>> Login([FromBody] UserCredentials userCredentials)
+        {
+            var result = await signInManager.PasswordSignInAsync(userCredentials.Email,
+                userCredentials.Password, isPersistent: false, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return await BuildToken(userCredentials);
+            }
+            else
+            {
+                return BadRequest("Incorrect Login");
+            }
+        }
+
+        private async Task<AuthenticationResponse> BuildToken(UserCredentials userCredentials)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim("email", userCredentials.Email)
+            };
+
+            var user = await userManager.FindByNameAsync(userCredentials.Email);
+            var claimsDB = await userManager.GetClaimsAsync(user);
+
+            claims.AddRange(claimsDB);
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["keyjwt"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expiration = DateTime.UtcNow.AddYears(1);
+
+            var token = new JwtSecurityToken(issuer: null, audience: null, claims: claims,
+                expires: expiration, signingCredentials: creds);
+
+            return new AuthenticationResponse()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiration
+            };
         }
 
     }
