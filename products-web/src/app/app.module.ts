12d72f4477c05744ec31b68ba7f5ec84aadf7df5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-12-26 20:50:09 +0530

    Update app.module.ts

--- products-web/src/app/app.module.ts
+++ products-web/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './modules/shared/shared.module';
+import { NgmaterialModule } from './ngmaterial/ngmaterial/ngmaterial.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { SharedModule } from './modules/shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    NgmaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
