5f0f8b67a742538b1f3c3e2b7a0082bf60f2c460
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-12-26 20:44:07 +0530

    Initial Version on Header and Footer

--- products-web/src/app/modules/shared/shared.module.ts
+++ products-web/src/app/modules/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
   ]
