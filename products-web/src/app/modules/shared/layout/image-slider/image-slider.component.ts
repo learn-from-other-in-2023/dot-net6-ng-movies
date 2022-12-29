c19d91333f2942d384190471d1ef257c34a858e3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-12-29 17:58:14 +0530

    Image Slider

--- products-web/src/app/modules/shared/layout/image-slider/image-slider.component.ts
+++ products-web/src/app/modules/shared/layout/image-slider/image-slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { ISlide } from '~/app/interfaces/islide.interface';
 
 @Component({
@@ -6,9 +6,54 @@ import { ISlide } from '~/app/interfaces/islide.interface';
   templateUrl: './image-slider.component.html',
   styleUrls: ['./image-slider.component.scss']
 })
-export class ImageSliderComponent {
+export class ImageSliderComponent implements OnInit, OnDestroy {
 
   @Input()
   slides: ISlide[] = [];
+
+  currentIndex: number = 0;
+  timeoutId?: number;
+
+  ngOnInit(): void {
+    this.resetTimer();
+  }
+
+  ngOnDestroy() {
+    window.clearTimeout(this.timeoutId);
+  }
+
+  resetTimer() {
+    if (this.timeoutId) {
+      window.clearTimeout(this.timeoutId);
+    }
+    this.timeoutId = window.setTimeout(() => this.goToNext(), 3000);
+  }
+
+  goToPrevious(): void {
+    const isFirstSlide = this.currentIndex === 0;
+    const newIndex = isFirstSlide
+      ? this.slides.length - 1
+      : this.currentIndex - 1;
+
+    this.resetTimer();
+    this.currentIndex = newIndex;
+  }
+
+  goToNext(): void {
+    const isLastSlide = this.currentIndex === this.slides.length - 1;
+    const newIndex = isLastSlide ? 0 : this.currentIndex + 1;
+
+    this.resetTimer();
+    this.currentIndex = newIndex;
+  }
+
+  goToSlide(slideIndex: number): void {
+    this.resetTimer();
+    this.currentIndex = slideIndex;
+  }
+
+  getCurrentSlideUrl() {
+    return `url('${this.slides[this.currentIndex].thumbnailUrl}')`;
+  }
 
 }
