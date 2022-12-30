a67bc323a3796f3d59ee5002790fed506ed6368c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-12-30 09:07:39 +0530

    Swamy/28dec2022 work (#30)
    * Update header.component.html
    
    * Face lift of Header
    
    * Face lift
    
    * Update products-list.component.html
    
    * UI Face lift
    
    * Changes
    
    * Changes
    
    * Message Band
    
    * Message band facelift
    
    * Image slider Initial Version
    
    * Changes
    
    * Image Slider

--- products-web/src/app/modules/home/home.component.ts
+++ products-web/src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ISlide } from '~/app/interfaces/islide.interface';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  slides: ISlide[] = [
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/1/thumbnail.jpg', title: 'iPhone 9' },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/2/thumbnail.jpg', title: 'iPhone X' },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/3/thumbnail.jpg', title: 'Samsung Universe 9' },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/4/thumbnail.jpg', title: 'OPPOF19' },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/5/thumbnail.jpg', title: 'Huawei P30' },
+  ];
 
 }
