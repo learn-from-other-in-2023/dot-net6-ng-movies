58c4b84e71d77484ebb43fde6d0c9e9662bbd363
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-02 18:56:11 +0530

    Data

--- products-web/src/app/modules/home/home.component.ts
+++ products-web/src/app/modules/home/home.component.ts
@@ -11,17 +11,18 @@ export class HomeComponent {
 
   slides: ISlide[] = [
     { thumbnailUrl: 'https://i.dummyjson.com/data/products/1/thumbnail.jpg', title: 'iPhone 9' },
-    { thumbnailUrl: 'https://i.dummyjson.com/data/products/6/thumbnail.jpg', title: 'MacBook Pro' },
     { thumbnailUrl: 'https://i.dummyjson.com/data/products/17/thumbnail.jpg', title: 'Tree Oil 30ml' },
-    { thumbnailUrl: 'https://i.dummyjson.com/data/products/22/thumbnail.jpg', title: 'Elbow Macaroni - 400 gm' },
-    { thumbnailUrl: 'https://i.dummyjson.com/data/products/25/thumbnail.jpg', title: 'Gulab Powder 50 Gram' },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/21/thumbnail.png', title: 'Daal Masoor' },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/22/thumbnail.jpg', title: 'Elbow Macaroni' },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/26/thumbnail.jpg', title: 'Plant Hanger' },
   ];
 
   featuredProducts: IProductQuickView[] = [
-    { thumbnailUrl: 'https://i.dummyjson.com/data/products/6/thumbnail.jpg', title: 'MacBook Pro', price: 10.98 },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/1/thumbnail.jpg', title: 'iPhone 9', price: 10.98 },
     { thumbnailUrl: 'https://i.dummyjson.com/data/products/17/thumbnail.jpg', title: 'Tree Oil 30ml', price: 20.23 },
-    { thumbnailUrl: 'https://i.dummyjson.com/data/products/22/thumbnail.jpg', title: 'Elbow Macaroni - 400 gm', price: 47.45 },
-    { thumbnailUrl: 'https://i.dummyjson.com/data/products/25/thumbnail.jpg', title: 'Gulab Powder 50 Gram', price: 67.33 },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/21/thumbnail.png', title: 'Daal Masoor', price: 10.98 },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/22/thumbnail.jpg', title: 'Elbow Macaroni', price: 47.45 },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/26/thumbnail.jpg', title: 'Plant Hanger', price: 67.33 },
   ]
 
   constructor() { }
