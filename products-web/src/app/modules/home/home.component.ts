1ea7687aababfaa41fd378e1d69f7bc1595727c7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-02 18:49:54 +0530

    Update home.component.ts

--- products-web/src/app/modules/home/home.component.ts
+++ products-web/src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProductQuickView } from '~/app/interfaces/iproduct-quickview';
 import { ISlide } from '~/app/interfaces/islide.interface';
 
 @Component({
@@ -10,10 +11,21 @@ export class HomeComponent {
 
   slides: ISlide[] = [
     { thumbnailUrl: 'https://i.dummyjson.com/data/products/1/thumbnail.jpg', title: 'iPhone 9' },
-    { thumbnailUrl: 'https://i.dummyjson.com/data/products/2/thumbnail.jpg', title: 'iPhone X' },
-    { thumbnailUrl: 'https://i.dummyjson.com/data/products/3/thumbnail.jpg', title: 'Samsung Universe 9' },
-    { thumbnailUrl: 'https://i.dummyjson.com/data/products/4/thumbnail.jpg', title: 'OPPOF19' },
-    { thumbnailUrl: 'https://i.dummyjson.com/data/products/5/thumbnail.jpg', title: 'Huawei P30' },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/6/thumbnail.jpg', title: 'MacBook Pro' },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/17/thumbnail.jpg', title: 'Tree Oil 30ml' },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/22/thumbnail.jpg', title: 'Elbow Macaroni - 400 gm' },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/25/thumbnail.jpg', title: 'Gulab Powder 50 Gram' },
   ];
+
+  featuredProducts: IProductQuickView[] = [
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/6/thumbnail.jpg', title: 'MacBook Pro', price: 10.98 },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/17/thumbnail.jpg', title: 'Tree Oil 30ml', price: 20.23 },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/22/thumbnail.jpg', title: 'Elbow Macaroni - 400 gm', price: 47.45 },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/25/thumbnail.jpg', title: 'Gulab Powder 50 Gram', price: 67.33 },
+  ]
+
+  constructor() { }
+
+
 
 }
