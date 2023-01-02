209db19a8394d6496c2d70ba2e2a66ae38a68b25
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-02 19:34:37 +0530

    Swamy/01jan2023 work (#33)
    * Update header.component.html
    
    * Update home.component.html
    
    * Featured Products
    
    * Code Refactoring
    
    * Update home.component.ts
    
    * Data
    
    * Product Quick View
    
    * Displaying Products
    
    * UI Facelift

--- products-web/src/app/modules/home/home.component.ts
+++ products-web/src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProductQuickView } from '~/app/interfaces/iproduct-quickview';
 import { ISlide } from '~/app/interfaces/islide.interface';
 
 @Component({
@@ -10,10 +11,22 @@ export class HomeComponent {
 
   slides: ISlide[] = [
     { thumbnailUrl: 'https://i.dummyjson.com/data/products/1/thumbnail.jpg', title: 'iPhone 9' },
-    { thumbnailUrl: 'https://i.dummyjson.com/data/products/2/thumbnail.jpg', title: 'iPhone X' },
-    { thumbnailUrl: 'https://i.dummyjson.com/data/products/3/thumbnail.jpg', title: 'Samsung Universe 9' },
-    { thumbnailUrl: 'https://i.dummyjson.com/data/products/4/thumbnail.jpg', title: 'OPPOF19' },
-    { thumbnailUrl: 'https://i.dummyjson.com/data/products/5/thumbnail.jpg', title: 'Huawei P30' },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/17/thumbnail.jpg', title: 'Tree Oil 30ml' },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/21/thumbnail.png', title: 'Daal Masoor' },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/22/thumbnail.jpg', title: 'Elbow Macaroni' },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/26/thumbnail.jpg', title: 'Plant Hanger' },
   ];
+
+  featuredProducts: IProductQuickView[] = [
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/1/thumbnail.jpg', title: 'iPhone 9', price: 10.98 },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/17/thumbnail.jpg', title: 'Tree Oil 30ml', price: 20.23 },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/21/thumbnail.png', title: 'Daal Masoor', price: 10.98 },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/22/thumbnail.jpg', title: 'Elbow Macaroni', price: 47.45 },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/26/thumbnail.jpg', title: 'Plant Hanger', price: 67.33 },
+  ]
+
+  constructor() { }
+
+
 
 }
