34f31de2f0da4a80de5088d842c26cb80982fb54
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-03 22:42:20 +0530

    Enhancements

--- products-web/src/app/modules/products/products-list/products-list.component.ts
+++ products-web/src/app/modules/products/products-list/products-list.component.ts
@@ -14,11 +14,11 @@ export class ProductsListComponent {
     { thumbnailUrl: 'https://i.dummyjson.com/data/products/12/thumbnail.jpg', title: 'Daal Masoor', price: 10.98 },
     { thumbnailUrl: 'https://i.dummyjson.com/data/products/17/thumbnail.jpg', title: 'Elbow Macaroni', price: 47.45 },
     { thumbnailUrl: 'https://i.dummyjson.com/data/products/22/thumbnail.jpg', title: 'Plant Hanger', price: 67.33 },
-    { thumbnailUrl: 'https://i.dummyjson.com/data/products/27/thumbnail.png', title: 'iPhone 9', price: 10.98 },
-    { thumbnailUrl: 'https://i.dummyjson.com/data/products/17/thumbnail.jpg', title: 'Tree Oil 30ml', price: 20.23 },
-    { thumbnailUrl: 'https://i.dummyjson.com/data/products/21/thumbnail.png', title: 'Daal Masoor', price: 10.98 },
-    { thumbnailUrl: 'https://i.dummyjson.com/data/products/22/thumbnail.jpg', title: 'Elbow Macaroni', price: 47.45 },
-    { thumbnailUrl: 'https://i.dummyjson.com/data/products/26/thumbnail.jpg', title: 'Plant Hanger', price: 67.33 },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/26/thumbnail.jpg', title: 'iPhone 9', price: 10.98 },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/33/thumbnail.jpg', title: 'Tree Oil 30ml', price: 20.23 },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/36/thumbnail.jpg', title: 'Daal Masoor', price: 10.98 },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/43/thumbnail.jpg', title: 'Elbow Macaroni', price: 47.45 },
+    { thumbnailUrl: 'https://i.dummyjson.com/data/products/46/thumbnail.jpg', title: 'Plant Hanger', price: 67.33 },
   ]
   
 }
